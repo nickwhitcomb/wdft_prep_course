65c8d8f00f22e78b6cb701dc67c280ace8d40ef5
nickwhitcomb <nickwhitcomb@Nicks-MacBook-Pro.local>
2017-11-23 13:07:25 -0800

    Hands On Challenge 1 Added

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -23,3 +23,25 @@ Write a loop that makes seven calls to console.log to output the following trian
 for (var triangle = "#"; triangle.length < 8; triangle += "#"){
     console.log(triangle);
 }
+
+/*
+Further Exercises
+Hands On Challenges Below
+*/
+
+//Exercise 1
+
+{
+    for (var i = 1; i < 101; i++)
+    if (i % 5 == 0 && i % 3 != 0){
+        console.log("buzz");
+    }
+    else if (i % 3 == 0 && i % 5 != 0){
+        console.log("fizz");
+    }
+    else if (i % 5 == 0 && i % 3 == 0){
+        console.log("fizzbuzz");
+    }
+    else { console.log(i);
+    } 
+};
